b1868e07105450107247c7c02fdba8dff22d6833
siof <siof@localhost>
2012-06-08 12:19:16 +0200

    push world.sql file updated to latest core/db rev

--- structures/world_struct.sql
+++ structures/world_struct.sql
@@ -2524,7 +2524,7 @@ CREATE TABLE `waypoint_data` (
   `position_y` float NOT NULL DEFAULT '0',
   `position_z` float NOT NULL DEFAULT '0',
   `delay` int(10) unsigned NOT NULL DEFAULT '0',
-  `move_flag` tinyint(1) NOT NULL DEFAULT '0',
+  `move_type` tinyint(1) NOT NULL DEFAULT '0',
   `action` int(11) NOT NULL DEFAULT '0',
   `action_chance` smallint(3) NOT NULL DEFAULT '100',
   `wpguid` int(11) NOT NULL DEFAULT '0'
