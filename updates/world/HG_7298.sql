82c54efd3fbbe858dd1f3e0cb4092b73b568ea93
zlokobni <zlokobni@localhost>
2013-03-29 20:52:33 +0200

    HG_7299.

--- updates/world/HG_7298.sql
+++
@@ -1,1 +0,0 @@
-UPDATE `creature` SET `spawntimesecs` = '63' WHERE `id` IN (23448);
---
+++ updates/world/HG_7299.sql
@@ -0,0 +1,6 @@
+UPDATE `creature` SET `spawntimesecs` = '25' WHERE `id` IN (23448);
+DELETE FROM `spell_script_target` WHERE `entry` IN (39635,39849);
+INSERT INTO `spell_script_target` VALUES
+(39849,1,23448),
+(39635,1,22917);
+
