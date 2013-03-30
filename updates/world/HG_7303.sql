f0da32c7a41cf378f00bf13bec7bcf35c86cce87
zlokobni <zlokobni@localhost>
2013-03-30 13:35:37 +0200

    HG_7303.

--- updates/world/HG_7303.sql
+++ updates/world/HG_7303.sql
@@ -3,4 +3,4 @@ DELETE FROM `spell_script_target` WHERE `entry` IN (39635,39849);
 INSERT INTO `spell_script_target` VALUES
 (39849,1,23448),
 (39635,1,22917);
-
+DELETE FROM `scripted_spell_id` WHERE `id` = 39635;
