e98065f16e08e75d6357cc00b107478b426eefe8
siof <siof@localhost>
2013-05-14 20:24:09 +0200

    xp fix ?

--- updates/world/HG_7409.sql
+++ updates/world/HG_7409.sql
@@ -1,5 +1,5 @@
 ALTER TABLE creature_template
-	ADD COLUMN xp_modifier FLOAT NOT NULL DEFAULT 1.0;
+	ADD COLUMN xp_modifier FLOAT NOT NULL DEFAULT 1.0 AFTER armor;
 
 -- 0 exp modifier for NPC with CREATURE_FLAG_EXTRA_NO_XP_AT_KILL flag and add to 'fishes' in netherstorm
 UPDATE creature_template SET xp_modifier = 0.0 WHERE entry = 19731;
