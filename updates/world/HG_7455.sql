3c2b196db681730ea9e4a358535fcf934b8d11f9
koostosh <koostosh@localhost>
2013-06-10 17:04:31 +0200

    HG_7455 [FS#671]

---
+++ updates/world/HG_7455.sql
@@ -0,0 +1,3 @@
+update quest_template set rewrepvalue2=500,rewrepvalue3=0, rewrepvalue4=0, rewrepvalue5=0,rewrepfaction3=0, rewrepfaction4=0, rewrepfaction5=0 where entry in ( 9266, 9267, 9268, 9259);
+update quest_template set rewrepfaction2= 21 where entry=9259;
+update quest_template set rewrepfaction2= 577 where entry=9266;
