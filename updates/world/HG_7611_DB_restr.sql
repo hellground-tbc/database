2552f1022358b5d861687c3a50e99731a279aaa4
koostosh <koostosh@localhost>
2013-08-28 10:58:35 +0200

    restructurization restructurization ;p

--- updates/world/HG_7611_DB_restr.sql
+++ updates/world/HG_7611_DB_restr.sql
@@ -1,30 +1,30 @@
 ALTER TABLE `command`
   CHANGE security permission_mask BIGINT UNSIGNED NOT NULL DEFAULT 1;
 
+--    lvl 5->
 UPDATE `command`
-SET `permission_mask` = `permission_mask` | 0x000001
-WHERE `permission_mask` = 0;
-
-UPDATE `command`
-SET `permission_mask` = `permission_mask` | 0x000101
-WHERE `permission_mask` = 1;
-
---    lvl 2
-UPDATE `command`
-SET `permission_mask` = `permission_mask` | 0x000301
-WHERE `permission_mask` = 2;
-
---    lvl 3
-UPDATE `command`
-SET `permission_mask` = `permission_mask` | 0x000B03
-WHERE `permission_mask` = 3;
+SET `permission_mask` = 0x002801
+WHERE `permission_mask` > 4;
 
 --    lvl 4
 UPDATE `command`
-SET `permission_mask` = `permission_mask` | 0x001B03
+SET `permission_mask` = 0x001801
 WHERE `permission_mask` = 4;
 
---    lvl 5->
+--    lvl 3
 UPDATE `command`
-SET `permission_mask` = `permission_mask` | 0x003B03
-WHERE `permission_mask` > 4;
+SET `permission_mask` = 0x000801
+WHERE `permission_mask` = 3;
+
+--    lvl 2
+UPDATE `command`
+SET `permission_mask` = 0x000201
+WHERE `permission_mask` = 2;
+
+UPDATE `command`
+SET `permission_mask` = 0x000101
+WHERE `permission_mask` = 1;
+
+UPDATE `command`
+SET `permission_mask` = 0x000001
+WHERE `permission_mask` = 0;
