dc1ca5e73126995f72e691f61bc5900c416fb019
Siarkowy <siarkowy@siarkowy.net>
2013-11-17 21:18:00 +0100

    Add missing semicolons for SQL combiner

--- updates/world/HG_7672.sql
+++ updates/world/HG_7672.sql
@@ -1,2 +1,2 @@
 update creature_loot_template set condition_value2 = 10802 where item=31754;
-update creature_loot_template set condition_value1 = 10637,condition_value2 = 10688 where item=30811
+update creature_loot_template set condition_value1 = 10637,condition_value2 = 10688 where item=30811;
