5688ebca825ffa6c2b124a2eb5c5512126b0e00e
Siarkowy <siarkowy@siarkowy.net>
2013-11-17 21:09:58 +0100

    Remove redundant USE statement

--- structures/realm_struct.sql
+++ structures/realm_struct.sql
@@ -12,7 +12,6 @@ MySQL - 10.0.4-MariaDB : Database - realmd
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-USE `realmd`;
 
 /*Table structure for table `account` */
 
