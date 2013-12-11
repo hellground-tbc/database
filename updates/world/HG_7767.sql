5de6f85b27befe1630c5e080252d55c3932c030d
koostosh <koostosh@gmail.com>
2013-12-11 17:13:43 +0100

    HG_7767

---
+++ updates/world/HG_7767.sql
@@ -0,0 +1,4 @@
+Insert into hellground_string values
+('352', 'Trollmute history for account %s', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('353', 'Account %s has never been trollmuted', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+('354', 'GM %s trollmuted %s for %u minutes. Reason: %s', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
