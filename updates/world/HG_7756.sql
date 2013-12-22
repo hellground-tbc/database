d40808b6bcd4f0e962c53ae45ffe8d6946c77848
Sharm <for.sneg@gmail.com>
2013-12-23 01:32:23 +0400

    CRLF->LF + fix semicolon

--- updates/world/HG_7756.sql
+++ updates/world/HG_7756.sql
@@ -4,4 +4,4 @@ Insert into hellground_string values
 ('288', 'Whispering to GM activated for %s', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
 ('289', 'Whispering to GM deactivated for %s', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 update command set `help`='Syntax: .whispers [name] on|off
-Enable/disable accepting whispers by GM, if name provided enable/disable player ability to whisper with GM.' where `name`='whispers'
+Enable/disable accepting whispers by GM, if name provided enable/disable player ability to whisper with GM.' where `name`='whispers';
