b9665bf11000f1508c29118fc5e16175c7c3481b
Aboretlan <aboretlan@hellground.net>
2014-02-07 21:42:45 +0000

    missing lines from cleanup ids.

--- tools/cleanup_instance_ids.sql
+++ tools/cleanup_instance_ids.sql
@@ -3,5 +3,6 @@ CREATE TABLE `instance_helper` (`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,`o
 UPDATE `character_instance` ci JOIN instance_helper ih ON ci.instance = ih.oldid SET ci.instance = ih.id;
 UPDATE `group_instance` ci JOIN instance_helper ih ON ci.instance = ih.oldid SET ci.instance = ih.id;
 UPDATE `instance` ci JOIN instance_helper ih ON ci.id = ih.oldid SET ci.id = ih.id;
-
+UPDATE `creature_respawn` ci JOIN instance_helper ih ON ci.instance = ih.oldid SET ci.instance = ih.id;
+UPDATE `gameobject_respawn` ci JOIN instance_helper ih ON ci.instance = ih.oldid SET ci.instance = ih.id;
 DROP TABLE `instance_helper`;
