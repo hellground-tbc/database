dceab9ecc30031dbedb098baf5e08816ff5ab05a
Siarkowy <siarkowy@siarkowy.net>
2014-03-09 21:14:45 +0100

    Add missing semicolons

--- updates/world/HG_7880_DB_443.sql
+++ updates/world/HG_7880_DB_443.sql
@@ -1,1 +1,1 @@
-update creature_template set minhealth= 832750, maxhealth= 832750 where entry=15625
+update creature_template set minhealth= 832750, maxhealth= 832750 where entry=15625;
--- updates/world/HG_7939.sql
+++ updates/world/HG_7939.sql
@@ -1,2 +1,2 @@
 Insert into hellground_string value 
-('11017', '|cff00ff00Player %s (%u) killed by instakill guardian, position X: %f Y: %f Z: %f Map: %u|r', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
+('11017', '|cff00ff00Player %s (%u) killed by instakill guardian, position X: %f Y: %f Z: %f Map: %u|r', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
