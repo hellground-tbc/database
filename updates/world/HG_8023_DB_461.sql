c9c834807b4743a464e97a51bf63a35a9ab06bed
Gwelyan <gwelyan@hellground.net>
2014-04-08 00:15:33 +0000

    HG_8023_DB_461 #000184 fix

---
+++ updates/world/HG_8023_DB_461.sql
@@ -0,0 +1,2 @@
+insert into npc_text (ID, text0_0) values (3,"I do so enjoy sitting out here, listening to the sounds of the Great Sea. Oh, what? Yes, there is a ship that will take you to Menethil Harbor. It docks here from time to time.");
+insert into npc_gossip values (8464,3);
