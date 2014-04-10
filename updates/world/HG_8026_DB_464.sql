dc15b86b4ecf627d34cb58baf1b8597a70000f6b
Elyrion <Elyrion@localhost>
2014-04-10 13:57:36 +0200

    The Arcatraz: more visuals

---
+++ updates/world/HG_8026_DB_464.sql
@@ -0,0 +1,3 @@
+UPDATE `creature_template` SET `InhabitType` = '4' WHERE `entry` = '21439';
+UPDATE `creature_template` SET `InhabitType` = '4' WHERE `entry` = '21437';
+UPDATE `creature_template` SET `InhabitType` = '4' WHERE `entry` = '21438';
